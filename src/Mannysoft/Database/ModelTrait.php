11787182ff66e3e3bb3fa328e089f5fe1fedecaf
Manny Isles <mannysoft@gmail.com>
2015-01-26 17:25:39 +0800

    Update ModelTrait.php

--- src/Mannysoft/Database/ModelTrait.php
+++ src/Mannysoft/Database/ModelTrait.php
@@ -2,16 +2,12 @@
 
 trait ModelTrait {
 
-	public function looksGood() 
-	{
-	 	return ShipperConsignee::where('type', '=', 'consignee')
-                ->where('user_id', '=', 1)->get();
-	}
+	
 
 	public function scopeSortBy($query, $field, $order)
-    {
-        if($field == '' and $order == '') return;
-        return $query->orderBy($field, $order);
-    }
+    	{
+	        if($field == '' and $order == '') return;
+	        return $query->orderBy($field, $order);
+    	}
 
 }
